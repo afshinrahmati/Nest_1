d9fcff3e6fbd7493b5589969a7920ad4b827e273
a.rahmati <a.rahmati@kianiranian.com>
2023-04-13 16:00:29 +0330

    extra file deleted

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-
-async function bootStrap(params: any) {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootStrap("op");
