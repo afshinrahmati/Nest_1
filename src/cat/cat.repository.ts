2c908618840ea57f6805e1fb94f13c1c857e3e75
a.rahmati <a.rahmati@kianiranian.com>
2023-04-15 10:47:42 +0330

    repository

--- src/cat/cat.repository.ts
+++ src/cat/cat.repository.ts
@@ -1,13 +1,24 @@
-import { readFile,writeFile } from "fs/promises";
+import { readFile, writeFile } from "fs/promises";
 
-export class MessagesRepository {
-    async findOne(id:string){
+export class CatRepository {
+  async findOne(id: string) {
+    const contents = await readFile("cat-information.json", "utf8");
+    const cats = JSON.parse(contents);
+    return cats[id];
+  }
+  async findAll() {
+    const contents = await readFile("cat-information.json", "utf8");
+    const cats = JSON.parse(contents);
+    return cats;
+  }
 
-    }
-    async findAll() {
+  async create({ name: string, age: number }) {
+    const contents = await readFile("cat-information.json", "utf8");
+    const cats = JSON.parse(contents);
 
-    }
+    const id = Math.floor(Math.random() * 999);
 
-    async create({name:string,age:number}){
-    }
+    cats[id] = { id, contents };
+    await writeFile("cat-information.json", JSON.stringify(cats));
+  }
 }
